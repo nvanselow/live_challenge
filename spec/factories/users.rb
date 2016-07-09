dbedf8092287ecabe743e6b805ef50454bdeccec
Nick V <nvanselow@users.noreply.github.com>
2016-07-09 18:00:45 -0400

    Revert "Setup devise"

--- spec/factories/users.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    sequence(:display_name) { |num| "user_#{num}" }
-    sequence(:email) { |num| "email_#{num}@mailinator.com" }
-    password 'password'
-  end
-end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,9 +5,6 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-require 'capybara/rails'
-require 'valid_attribute'
-
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -58,7 +55,10 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+require "capybara/rails"
+require "valid_attribute"
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 end
+
