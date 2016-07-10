af76fc4c1967b928b46f5ece17c67ce7f45a2abb
Nick(Mac) <nvanselow@hotmail.com>
2016-07-10 13:38:15 -0400

    Add test for all_for method on questions

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -8,4 +8,22 @@ describe Question, type: :model do
   it { should_not have_valid(:body).when('', nil) }
 
   it { should belong_to(:user) }
+
+  describe '.all_for' do
+    it 'gets all the questions made by a specified user' do
+      user = FactoryGirl.create(:user)
+      user_questions = FactoryGirl.create_list(:question, 3, user: user)
+      another_users_questions = FactoryGirl.create_list(:question, 2)
+
+      questions = Question.all_for(user)
+
+      user_questions.each do |question|
+        expect(questions).to include(question)
+      end
+
+      another_users_questions.each do |question|
+        expect(questions).not_to include(question)
+      end
+    end
+  end
 end
