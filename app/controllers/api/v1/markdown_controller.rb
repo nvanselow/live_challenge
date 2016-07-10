142b4c24eca11b361415913f4f58e657a258e54e
Nick(Mac) <nvanselow@hotmail.com>
2016-07-10 14:55:27 -0400

    Create markdown api

--- app/controllers/api/v1/markdown_controller.rb
+++ app/controllers/api/v1/markdown_controller.rb
@@ -1,7 +1,9 @@
 require 'markdownable'
 
 class Api::V1::MarkdownController < ApplicationController
+  include Markdownable
+
   def create
-    
+    render json: { html: parse_markdown(params[:text]) }, status: :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'homepage#index'
 
+  resource :markdown, only: [:create], controller: 'markdown'
   resources :questions, only: [:index, :show, :new, :create]
 end
