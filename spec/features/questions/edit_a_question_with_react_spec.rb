dc35f489620b9271c7e457bbd283cea5ec1d83ca
Nick(Mac) <nvanselow@hotmail.com>
2016-07-10 22:59:36 -0400

    Ensure old body appears on edit form

---
+++ spec/features/questions/edit_a_question_with_react_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'edit a question with react component', %{
+  As a user
+  I want to edit a question using a react component
+  So that I can preview my question in processed markdown
+}, js: true do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:question) { FactoryGirl.create(:question) }
+
+  before do
+    sign_in_as(user)
+  end
+
+  scenario 'user can make changes to previous body of question' do
+    visit edit_question_path(question)
+
+    expect(page).to have_content(question.body)
+  end
+end
