8bf4b77d91e5831e959f399353d8591c67121aa8
Nick(Mac) <nvanselow@hotmail.com>
2016-07-10 23:03:06 -0400

    Update acceptance critera completion

--- spec/features/questions/view_questions_index_spec.rb
+++ spec/features/questions/view_questions_index_spec.rb
@@ -6,11 +6,11 @@ feature 'view the questions index', %{
   So that I can see all the questions I have created
 } do
   # Acceptance Criteria
-  # [ ] User can navigate to the questions index from the homepage
-  # [ ] User can navigate to the questions index from the nav bar
-  # [ ] User views all of their own questions
-  # [ ] User cannot see a question created by a different user
-  # [ ] Only authenticated users can view the questions index
+  # [X] User can navigate to the questions index from the homepage
+  # [X] User can navigate to the questions index from the nav bar
+  # [X] User views all of their own questions
+  # [X] User cannot see a question created by a different user
+  # [X] Only authenticated users can view the questions index
 
   let(:user) { FactoryGirl.create(:user) }
   let!(:questions) { FactoryGirl.create_list(:question, 3, user: user) }
