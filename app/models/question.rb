5164e5a8cf5ea311b8aa0d9d879c4db30482a096
Nick(Mac) <nvanselow@hotmail.com>
2016-07-14 08:44:21 -0400

    Add answer associations to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ require 'securerandom'
 
 class Question < ActiveRecord::Base
   belongs_to :user
+  has_many :answers, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true
