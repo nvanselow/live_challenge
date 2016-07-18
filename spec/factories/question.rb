0abeb175b843333f78dcd4ef349feb18b9f04020
Nick(Mac) <nvanselow@hotmail.com>
2016-07-18 10:06:45 -0400

    Add factory for code questions

--- spec/factories/question.rb
+++ spec/factories/question.rb
@@ -4,9 +4,14 @@ FactoryGirl.define do
     body 'This is some text for a question I want to add.'
     long_id 'abcdefg'
     user
+    code_question false
 
     factory :markdown_question do
       body 'This body has some **markdown**.'
+    end
+
+    factory :code_question do
+      code_question true
     end
   end
 end
