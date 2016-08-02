8af21f06ae6ba5efbbb367d3a20a3c8d7872be25
Nick(Mac) <nvanselow@hotmail.com>
2016-08-02 09:37:58 -0400

    Add markdown to display code questions correctly

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,6 +4,10 @@ class AnswersController < ApplicationController
     @answer = Answer.new(answer_params)
     @answer.question = @question
 
+    if @question.code_question
+      @answer.body = '```ruby\n' + @answer.body + '\n```'
+    end
+
     if @answer.save
       flash[:success] = 'Your answer was submitted!'
     else
